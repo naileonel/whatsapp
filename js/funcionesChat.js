c8c9f35309d4614e9f70b0473007b7be1a355d01
naileonel <naibitleonel@gmail.com>
2016-06-14 08:46:26 -0500

    termino

--- js/funcionesChat.js
+++ js/funcionesChat.js
@@ -1,3 +1,4 @@
+/* mensajes */
 $(this).ready(function(){
 
 	$(".lista").click(function(){		
@@ -7,7 +8,7 @@ $(this).ready(function(){
 	});
 });
 
-
+/* enviar */
 function txtChat_KeyPress(e)
 {
 
@@ -22,5 +23,31 @@ function txtChat_KeyPress(e)
 }
 
 
+/* Funcion del Buscador de Palabras */
 
 
+
+$('.sub-menu').click(function() {
+  var data = $(this).attr('data')
+  var fotos = ["avatar.jpg", "logocodeacademy.png", "katy.jpg","jose.jpg", "andrea.jpg","aldo.jpg","mariapaula.jpg", "raymi.jpg", "rodulfo.jpg","anamaria.jpg", "mariana.jpg"];
+  var nombres = ["Alfredo Lopez", "Laboratoria Peru", "Katy Sanchez", "Mereganita Del Angel", "Andrea Lamas", "Aldo Alfaro", "Maria Paula Rivarola", "Raymi saldomando", "Rodulfo Prieto", "Ana Maria Martinez", "Mariana Costa"];
+  $('#foto').html('<img class="media-object " src="image/' +fotos[data]+ ' alt="...">');
+  $('#nombres').html('<h5>' +nombres[data]+ '</h5>');
+});
+
+function input() {
+  var llamarID = document.getElementById("#imagenPersona ");
+  return llamarID.value;
+}
+
+$('body').keyup(function enter(e) {
+  var agregar = input();
+    if (agregar !== '') {
+      if(e.keyCode == 13) {       
+        actual();
+        ObtenerLista(agregar);
+      clean();
+          $('#buscar-pal').submit();
+      }
+    }
+});
